17fec9c8cd428bb09a7f9c9c22631c7b3224ff42
Lans12345 <olanalans12345@gmail.com>
2023-11-09 13:41:59 +0800

    wip bulletin

--- lib/screens/bulletin_screen.dart
+++ lib/screens/bulletin_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:valley/widgets/button_widget.dart';
 import 'package:valley/widgets/text_widget.dart';
+
+import '../utils/routes.dart';
 
 class FacultyBulletinScreen extends StatelessWidget {
   final box = GetStorage();
@@ -36,7 +39,7 @@ class FacultyBulletinScreen extends StatelessWidget {
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     TextBold(
-                      text: 'Faculty Bulletin',
+                      text: 'CCS FACULTY BULLETIN OF SCHEDULE',
                       fontSize: 48,
                       color: Colors.white,
                     ),
@@ -44,6 +47,109 @@ class FacultyBulletinScreen extends StatelessWidget {
                 ),
                 const SizedBox(
                   height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        TextBold(
+                          text: 'Calendar',
+                          fontSize: 24,
+                          color: Colors.white,
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        const Card(
+                          child: SizedBox(
+                            height: 500,
+                            width: 400,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        TextBold(
+                          text: 'Special Announcement',
+                          fontSize: 24,
+                          color: Colors.white,
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        const Card(
+                          child: SizedBox(
+                            height: 500,
+                            width: 400,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        TextBold(
+                          text: 'Daily Reminders',
+                          fontSize: 24,
+                          color: Colors.white,
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Card(
+                          child: SizedBox(
+                            height: 500,
+                            width: 400,
+                            child: ListView.separated(
+                              itemCount: 20,
+                              separatorBuilder: (context, index) {
+                                return const Divider();
+                              },
+                              itemBuilder: (context, index) {
+                                return const ListTile();
+                              },
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ButtonWidget(
+                      color: Colors.black,
+                      label: 'CCS Faculty Schedule',
+                      onPressed: () {
+                        Navigator.pushReplacementNamed(
+                            context, Routes().homescreen);
+                      },
+                    ),
+                    ButtonWidget(
+                      color: Colors.black,
+                      label: 'View Laboratory Schedule',
+                      onPressed: () {},
+                    ),
+                    ButtonWidget(
+                      color: Colors.black,
+                      label: 'View Attendance',
+                      onPressed: () {
+                        Navigator.pushReplacementNamed(
+                            context, Routes().attendancescreen);
+                      },
+                    ),
+                  ],
                 ),
               ],
             ),
